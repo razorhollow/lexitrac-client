ecf197b274bcb0b0b86e31d7144e9a40164096aa
RazorHollow <rob@razorhollow.com>
2023-09-14 15:14:40 -0400

    wire login button to auth service

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import{ jsx, css } from '@emotion/react'
 import * as React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap-reboot.css'
 import Lottie from 'lottie-react'
 import logo from '../../assets/legal-document.json'
@@ -8,19 +9,52 @@ import { Button, Input, FormGroup } from '../../components/lib'
 import {Modal, ModalContents, ModalOpenButton} from '../../components/modal'
 import LoginPage from '../Login/Login'
 
-function LoginForm({onSubmit, submitButton}) {
-  function handleSubmit(event) {
-    event.preventDefault()
-    const {username, password} = event.target.elements
+// services
+import * as authService from '../../services/authService'
 
-    onSubmit({
-      username: username.value,
-      password: password.value,
-    })
+function LoginForm({ submitButton, handleAuthEvt }) {
+  const navigate = useNavigate()
+
+  const [message, setMessage] = React.useState('')
+  const [formData, setFormData] = React.useState({
+    email: '',
+    password: '',
+  })
+
+  const handleChange = event => {
+    setMessage('')
+    setFormData({ ...formData, [event.target.name]: event.target.value })
   }
+
+  const handleSubmit = async evt => {
+    evt.preventDefault()
+    console.log(formData)
+    try {
+      if (!import.meta.env.VITE_BACK_END_SERVER_URL) {
+        throw new Error('No VITE_BACK_END_SERVER_URL in front-end .env')
+      }
+      await authService.login(formData)
+      handleAuthEvt()
+      navigate('/')
+    } catch (err) {
+      console.log(err)
+      setMessage(err.message)
+    }
+  }
+
+  const { email, password } = formData
+
+  const isFormInvalid = () => {
+    return !(email && password)
+  }  
+
+  React.useEffect(() => {
+    console.log(formData)
+  })
   
   return (
     <form
+    autoComplete='off'
       css={{
         display: 'flex',
         flexDirection: 'column',
@@ -35,13 +69,13 @@ function LoginForm({onSubmit, submitButton}) {
     >
       <FormGroup>
         <label htmlFor="username">Username</label>
-        <Input id="username" />
+        <Input id="username" onChange={handleChange} type='text' value={email} name='email'/>
       </FormGroup>
       <FormGroup>
         <label htmlFor="password">Password</label>
-        <Input id="password" type="password" />
+        <Input id="password" type="password" onChange={handleChange} value={password} name='password'/>
       </FormGroup>
-      <div>{React.cloneElement(submitButton, {type: 'submit'})}</div>
+      <div>{React.cloneElement(submitButton, {type: 'submit', disabled: isFormInvalid() })}</div>
     </form>
     )
   }
